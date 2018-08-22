4ea1004d4b7d31cc97d7d053313985a882c97347
tina7ti <tina.ame77@gmail.com>
2018-08-22 23:07:00 +0200

    typing message_part7

--- index.js
+++ index.js
@@ -18,4 +18,8 @@ io.on('connection',function(socket){
 		io.sockets.emit("chat",data);
 	});
 
+	socket.on("typing",function (data) {
+		socket.broadcast.emit("typing",data);
+	});
+
 }); 
--- public/chat.js
+++ public/chat.js
@@ -10,6 +10,15 @@ btn.addEventListener("click",function(){
 	socket.emit("chat",{message:message.value,handle:handle.value});
 });
 
+message.addEventListener("keypress",function(){
+	socket.emit("typing",handle.value);
+});
+
 socket.on("chat",function (data) {
 	output.innerHTML+='<p><strong>' + data.handle + ': </strong>' + data.message + ' <hr></p>';
+	feedback.innerHTML ='';
+});
+
+socket.on("typing",function (data) {
+	feedback.innerHTML ='<p><em>' + data+' is typing a message ...<em></p>';
 });
