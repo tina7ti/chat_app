75df7be778dca45ad7c8a3f854d4ab89a73fd007
tina7ti <tina.ame77@gmail.com>
2018-08-22 22:52:33 +0200

    message sending_part6

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ var io = socket(server);
 
 io.on('connection',function(socket){
 
-	console.log(socket.id);
+	socket.on("chat",function (data) {
+		io.sockets.emit("chat",data);
+	});
 
 }); 
--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,15 @@
 var socket = io.connect("http://localhost:8000"); 
+
+var message = document.getElementById("message");
+var handle = document.getElementById("handle");
+var output= document.getElementById("output");
+var feedback= document.getElementById("feedback");
+var btn = document.getElementById("send");
+
+btn.addEventListener("click",function(){
+	socket.emit("chat",{message:message.value,handle:handle.value});
+});
+
+socket.on("chat",function (data) {
+	output.innerHTML+='<p><strong>' + data.handle + ': </strong>' + data.message + ' <hr></p>';
+});
