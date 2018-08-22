f3c2fb677d83a297473bca8264359ea2d5e3ac6a
tina7ti <tina.ame77@gmail.com>
2018-08-22 20:11:19 +0200

    starting using socket_part5

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ var server = app.listen(8000, function(){
 	console.log("the server is listening on the post 8000");
 
 });
+
+app.use(express.static('public'));
+
+var io = socket(server);
+
+io.on('connection',function(socket){
+
+	console.log(socket.id);
+
+}); 
